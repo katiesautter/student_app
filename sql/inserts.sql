ff59be03731f8d37450232fa6c3b16b691ad3ef0
Katie <katherine.e.sautter@lions.enc.edu>
2019-05-28 21:43:11 -0400

    file of insert statements

---
+++ sql/inserts.sql
@@ -0,0 +1,30 @@
+INSERT INTO users
+VALUES ('1', 'katiesautter', 'Horses12'); 
+
+INSERT INTO users
+VALUES ('2', 'babydragon', 'dragons10'); 
+
+INSERT INTO users
+VALUES ('3', 'beccathorne', 'buckyb'); 
+
+INSERT INTO posts
+VALUES ('1', 'public', 'Come to our Shrader Game Night', '2', '1'); 
+
+INSERT INTO posts
+VALUES ('2', 'public', 'Capture the Flag', '1', '2'); 
+
+INSERT INTO events
+VALUES ('1', 'Game Night', current_timestamp, '1'); 
+
+INSERT INTO events
+VALUES ('2', 'Capture the Flag', current_timestamp, '2'); 
+
+INSERT INTO location 
+VALUES ('1', 'Shrader');
+
+INSERT INTO location 
+VALUES ('2', 'Wolly lawn');
+
+INSERT INTO private_user 
+VALUES ('1', '2', '1');
+
