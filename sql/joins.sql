ef783a7fb04ff08d28f31c22f80e7da5fce4248d
Katie <katherine.e.sautter@lions.enc.edu>
2019-06-14 14:27:26 -0400

    zip file, and corrections to joins and schema

--- sql/joins.sql
+++ sql/joins.sql
@@ -17,7 +17,19 @@ WHERE users.username = 'babydragon';   --selects events babydragon has posted
 SELECT *
 from events 
 WHERE title = 'Capture the Flag'
-ORDER BY date_time ASC;    -- list posts about capture the flag in the order they were created
+ORDER BY date_time ASC;    -- list events about capture the flag in the order they were created
+
+SELECT *
+from posts JOIN 
+     events ON posts.fk_events_id=events.id
+WHERE title = 'Capture the Flag'
+ORDER BY posts.id ASC;   
+
+--^my corrected SQL statement for "list posts about capture the flag in the order they were created"
+--the statement below ordered them by the date_time of the events but an event can have multiple posts to it
+--so it needed to be ordered by the id of the posts instead which would put them in order of when the posts 
+--were created not the events 
+
 
 SELECT *
 from posts JOIN 
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,11 +1,35 @@
-CREATE TABLE users (id int, username varchar(50), password varchar(50));
+CREATE TABLE users (
+    id int, 
+    username varchar(50), 
+    password varchar(50)
+);  
 
-CREATE TABLE posts (id int, message varchar(1000), fk_user_id int, fk_events_id int);
+CREATE TABLE posts (
+    id int, 
+    message varchar(1000), 
+    fk_user_id int, 
+    fk_events_id int
+);
 
-CREATE TABLE events (id int, title varchar(150), status varchar(15), date_time TIMESTAMP, fk_location_id int);
+CREATE TABLE events (
+    id int, 
+    title varchar(150), 
+    status varchar(15), 
+    date_time TIMESTAMP, 
+    fk_location_id int
+);
 
-CREATE TABLE locations (id int, name varchar(100));
+CREATE TABLE locations (
+    id int, 
+    name varchar(100)
+);
 
-CREATE TABLE private_events_users (id int, fk_user_id int, fk_events_id int);
+CREATE TABLE private_events_users (
+    id int,    
+    fk_user_id int, 
+    fk_events_id int
+);
 
-
+--Multiline format is better because it makes the command more concise and easier 
+--to read/understand what the command is doing. also makes it easier to fix and
+--spot errors
