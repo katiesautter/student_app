0142ff11c25a1ee9fd3e01479a913177b3520b06
Katie <katherine.e.sautter@lions.enc.edu>
2019-06-16 16:59:03 -0400

    maven and bash fixes

--- bash/drop.sh
+++ bash/drop.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-psql -U ksautter -d sea <<EOF
-DROP TABLE users;
-DROP TABLE posts;
-DROP TABLE events;
-DROP TABLE locations;
-DROP TABLE private_events_users;
-EOF
+psql -U ksautter -d sea < /home/ksautter/src/sea/sql/drop.sh
---
+++ java/empty.java
@@ -0,0 +1,6 @@
+class empty {
+
+    public static void main(String args[]){
+        System.out.println("Empty Java Project");
+    }
+}
