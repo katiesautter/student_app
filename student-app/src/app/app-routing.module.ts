e1acef73d382254fa1e49dc09e344ee88e6790b0
Katie <katherine.e.sautter@lions.enc.edu>
2020-01-09 22:48:20 -0500

    added routing and routed to events

--- student-app/src/app/app-routing.module.ts
+++ student-app/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventDetailsComponent } from './event-details/event-details.component';
+import { EventsComponent } from './events/events.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'eventsdetail',
+    component: EventDetailsComponent,
+    data: { title: 'events' }
+  },
+
+  {
+    path: 'events',
+    component: EventsComponent,
+    data: { title: 'events' }
+  },
+
+  { path: '',
+    redirectTo: '/events',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- student-app/src/app/app.module.ts
+++ student-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventsComponent
+    EventsComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule,
